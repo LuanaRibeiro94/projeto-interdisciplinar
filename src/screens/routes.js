4c1f2e122cc910c5c0bf550366b2eeee9cb579f2
Luana Ribeiro <luanasribeiro94@gmail.com>
2019-06-01 18:38:34 -0300

    feat: Adiciona menu lateral e tela de orçamento

--- src/screens/routes.js
+++ src/screens/routes.js
@@ -1,11 +1,12 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createAppContainer, createDrawerNavigator, createStackNavigator } from 'react-navigation';
 import { Colors } from 'react-native-paper';
 import Home from './Home';
 import CadastroVeiculo from './CadastroVeiculo';
 import CadastroCliente from './CadastroCliente';
+import Orcamento from './Orcamento';
 import theme from '../configs/theme';
 
-const AppNavigator = createStackNavigator(
+const HomeStack = createStackNavigator(
   {
     Home: {
       screen: Home,
@@ -13,16 +14,78 @@ const AppNavigator = createStackNavigator(
         header: null,
       },
     },
+  },
+);
+
+const ClienteStack = createStackNavigator(
+  {
     CadastroCliente: {
       screen: CadastroCliente,
       navigationOptions: {
         title: 'Cadastro de cliente',
+        headerStyle: {
+          backgroundColor: theme.colors.primary,
+        },
+        headerTintColor: Colors.white,
       },
     },
+  },
+);
+
+const VeiculoStack = createStackNavigator(
+  {
     CadastroVeiculo: {
       screen: CadastroVeiculo,
       navigationOptions: {
         title: 'Cadastro de veículo',
+        headerStyle: {
+          backgroundColor: theme.colors.primary,
+        },
+        headerTintColor: Colors.white,
+      },
+    },
+  },
+);
+
+const OrcamentoStack = createStackNavigator(
+  {
+    Orcamento: {
+      screen: Orcamento,
+      navigationOptions: {
+        title: 'Solicitar orçamento',
+        headerStyle: {
+          backgroundColor: theme.colors.primary,
+        },
+        headerTintColor: Colors.white,
+      },
+    },
+  },
+);
+
+const AppNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: HomeStack,
+      navigationOptions: {
+        drawerLabel: 'Início',
+      },
+    },
+    Clientes: {
+      screen: ClienteStack,
+      navigationOptions: {
+        drawerLabel: 'Clientes',
+      },
+    },
+    CadastroVeiculo: {
+      screen: VeiculoStack,
+      navigationOptions: {
+        drawerLabel: 'Veículos',
+      },
+    },
+    Orcamento: {
+      screen: OrcamentoStack,
+      navigationOptions: {
+        drawerLabel: 'Orçamentos',
       },
     },
   },
