b662ea6b37afa223daf70b7cf13cd0b551daa475
Luana Ribeiro <luanasribeiro94@gmail.com>
2019-06-02 16:49:12 -0300

    feat: insere ícone e mensagem informativa na tela inicial

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Avatar, Colors, Title, Subheading } from 'react-native-paper';
 
 const Home = () => {
   return (
-    <View />
+    <View style={styles.container}>
+      <Avatar.Image
+        source={require('../assets/truck.png')}
+        size={100}
+        style={styles.avatar}
+      />
+      <Title>Nenhum veículo encontrado.</Title>
+      <Subheading>Por favor use o botão de menu para inserir um novo.</Subheading>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  avatar: {
+    backgroundColor: Colors.grey200,
+  },
+});
 
 export default Home;
