bab6273a1fb433e5895286a2d24ada00a38b9560
Luana Ribeiro <luanasribeiro94@gmail.com>
2020-03-17 21:03:33 -0300

    feat: Adiciona proptypes para size e color dos ícones

--- src/components/Icones/Agendamento.js
+++ src/components/Icones/Agendamento.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const AgendamentoIcone = ({ size, color }) => {
   return <MaterialCommunityIcons name="calendar-clock" size={size} color={color} />;
+};
+
+AgendamentoIcone.propTypes = {
+  size: PropTypes.number.isRequired,
+  color: PropTypes.string.isRequired,
 };
 
 export default AgendamentoIcone;
--- src/components/Icones/Servico.js
+++ src/components/Icones/Servico.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Foundation } from '@expo/vector-icons';
 
 const ServicoIcone = ({ size, color }) => {
   return <Foundation name="wrench" size={size} color={color} />;
+};
+
+ServicoIcone.propTypes = {
+  size: PropTypes.number.isRequired,
+  color: PropTypes.string.isRequired,
 };
 
 export default ServicoIcone;
