a1fba9a282514edb4d1ef7a1e6e7fc814552aa1f
Luana Ribeiro <luanasribeiro94@gmail.com>
2020-03-25 20:21:11 -0300

    chore: Configuração do firebase storage local

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -5,6 +5,7 @@ const config = {
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 };
 
 firebase.initializeApp(config);
