d271a797d9aebaaf9dbe76d3b7254dedee1bef0b
Luana Ribeiro <luanasribeiro94@gmail.com>
2020-05-10 21:10:06 -0300

    feat: Criação da tela Sobre Nós

--- src/components/Icones/index.js
+++ src/components/Icones/index.js
@@ -4,3 +4,4 @@ export { default as DespesaIcone } from './Despesa';
 export { default as OrcamentoIcone } from './Orcamento';
 export { default as ServicoIcone } from './Servico';
 export { default as LembreteIcone } from './Lembrete';
+export { default as SobreNosIcone } from './SobreNos';
---
+++ src/screens/SobreNos.js
@@ -0,0 +1,72 @@
+/* eslint-disable max-len */
+import React from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import {
+  Title, Subheading, Card, Divider, Caption,
+} from 'react-native-paper';
+import { FontAwesome, Foundation } from '@expo/vector-icons';
+
+const SobreNos = () => {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.card}>
+        <Card>
+          <Card.Title title="Sobre a oficina" titleStyle={styles.title} />
+          <Card.Cover
+            source={require('../assets/caminhao.jpg')}
+            style={styles.image}
+          />
+          <Card.Content>
+            <Caption style={{ textAlign: 'justify' }}>
+            A Riberscan combina experiência com versatilidade no atendimento, para oferecer toda a tecnologia
+            de ponta que seu Scania merece, com preços acessíveis. São anos dentro da própria concessionária
+            que seus fundadores acumularam para oferecer a você uma oficina mecânica completa e especializada
+            </Caption>
+            <Divider />
+            <View>
+              <Title style={styles.title}>Fale conosco</Title>
+              <View style={styles.info}>
+                <FontAwesome name="phone" size={24} color="#00BFFF" style={styles.icon} />
+                <Subheading>(16) 1234-5678</Subheading>
+              </View>
+              <View style={styles.info}>
+                <Foundation name="mail" size={24} color="#00BFFF" style={styles.icon} />
+                <Subheading>contato@teste.com</Subheading>
+              </View>
+            </View>
+          </Card.Content>
+        </Card>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    padding: 20,
+    marginTop: 20,
+  },
+  card: {
+    height: 350,
+  },
+  image: {
+    height: 230,
+  },
+  title: {
+    alignSelf: 'center',
+    color: '#00BFFF',
+  },
+  text: {
+    textAlign: 'justify',
+  },
+  info: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: 8,
+  },
+});
+
+export default SobreNos;
