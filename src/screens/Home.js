2c76f0e582721ee5983a583388820dc9c2de753b
Luana Ribeiro <luanasribeiro94@gmail.com>
2020-05-24 22:15:24 -0300

    feat: Altera tela inicial

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,24 +1,75 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Colors, Title } from 'react-native-paper';
+import {
+  View, StyleSheet, ScrollView, Linking,
+} from 'react-native';
+import {
+  Title, Card, Caption, Paragraph, Divider, Colors,
+} from 'react-native-paper';
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-      <Title style={styles.title}>Seja bem-vindo.</Title>
-    </View>
+    <ScrollView>
+      <View>
+        <Title style={styles.title}>Bem-vindo(a), Luana</Title>
+
+        <Title style={styles.title2}>Últimas notícias</Title>
+        <Card>
+          <Card.Title title="Pandemia" subtitle="21/05/2020"></Card.Title>
+          <Card.Cover
+            source={require('../assets/noticia1.png')}
+            style={styles.image}
+          />
+          <Card.Content>
+            <Paragraph style={styles.text}>
+              CNT discute com BNDES medidas econômicas para o transporte durante pandemia.
+            </Paragraph>
+            <Caption style={styles.link} onPress={() => Linking.openURL('https://www.cnt.org.br/agencia-cnt/cnt-discute-com-bndes-medidas-economicas-para-o-transporte-durante-pandemia')}>
+              Leia mais
+            </Caption>
+          </Card.Content>
+        </Card>
+      </View>
+      <Divider />
+      <View>
+        <Card>
+          <Card.Title title="Roubo de cargas" subtitle="21/05/2020"></Card.Title>
+          <Card.Cover
+            source={require('../assets/noticia2.png')}
+            style={styles.image}
+          />
+          <Card.Content>
+            <Paragraph style={styles.text}>
+              País registra mais de 18 mil casos de roubos de cargas em rodovias.
+            </Paragraph>
+            <Caption style={styles.link} onPress={() => Linking.openURL('https://www.cnt.org.br/agencia-cnt/pais-registra-mais-de-18-mil-casos-de-roubos-de-cargas-em-rodovias')}>
+              Leia mais
+            </Caption>
+          </Card.Content>
+        </Card>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   title: {
-    color: '#00CED1',
+    alignSelf: 'center',
+    color: '#00BFFF',
+    marginTop: 40,
+    paddingBottom: 20,
+  },
+  title2: {
+    margin: 15,
+    color: Colors.grey700,
+  },
+  image: {
+    height: 180,
+  },
+  link: {
+    color: 'blue',
+  },
+  text: {
+    textAlign: 'justify',
   },
 });
 
